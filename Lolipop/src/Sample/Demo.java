ddd8e2107b3acee2745cfd8fb340aa4816f83403
SANJAY V <sanjay3005v@gmail.com>
2025-05-12 09:42:56 +0530

    may 12

--- Lolipop/src/Sample/Demo.java
+++ Lolipop/src/Sample/Demo.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 public class Demo {
 	public static void main(String[] args) {
 		int[] arr = {1,2,3,4,5};
-		int[] nums = mulby2(arr);
-		System.out.println(Arrays.toString(nums));	
+		mulby2(arr);
+		System.out.println(Arrays.toString(arr));	
 	}
-	public static int[] mulby2(int[] nums) {
+	public static void mulby2(int[] nums) {
 		for(int i=0;i<nums.length;i++) {
 			nums[i]=nums[i]*2;
 		}
-		return nums;
+		return;
 	}
 }
