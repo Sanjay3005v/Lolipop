3114a81e466601bbe1d4e21e6601e8bd63b3154a
SANJAY V <sanjay3005v@gmail.com>
2025-05-13 11:03:37 +0530

    May 12

--- Lolipop/src/Sample/Demo2.java
+++ Lolipop/src/Sample/Demo2.java
@@ -1,10 +1,20 @@
 package Sample;
 
 public class Demo2 {
+	
+	public static void main(String[] args) {
+		System.out.println("main");
+		real a = new real();
+		real b = new real();
+
+	}
+	
+}
+class real{
 	static {
 		System.out.println("static");
 	}
-	public static void main(String[] args) {
-		System.out.println("main");
+	{
+		System.out.println("non-static");
 	}
 }
