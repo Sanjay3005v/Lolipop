66039c5c62ea52e0111c277102d0f58920003b5b
SANJAY V <sanjay3005v@gmail.com>
2025-05-15 10:55:46 +0530

    May 15

--- Lolipop/src/May15/Driver.java
+++ Lolipop/src/May15/Driver.java
@@ -3,8 +3,10 @@ package May15;
 public class Driver {
 	public static void main(String[] args) {
 		Branch branch = new Branch();
+		//setting values using setter methods
 		branch.setMN("Smith");
 		branch.setifsc("10121");
+		//printing values using getter method
 		System.out.println("Manager Name: "+branch.getMN());
 		System.out.println("ifsc code: "+branch.getifsc());
 	}
