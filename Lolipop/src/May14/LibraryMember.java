435b1becd5c803cbb4d1e68d7ff1866ea791c206
SANJAY V <sanjay3005v@gmail.com>
2025-05-15 09:38:14 +0530

    May 15

--- Lolipop/src/May14/LibraryMember.java
+++ Lolipop/src/May14/LibraryMember.java
@@ -1,22 +1,29 @@
 package May14;
 public class LibraryMember {
+	//static variable
 	static int totalMembers;
+	//non static variable
 	int id;
 	String name;
+	//static initializer
 	static {
 		System.out.println("Library Management System");
 	}
+	//non static initializer
 	{
 		System.out.println("Creating a new LibraryMember object...");
 	}
+	//constructor
 	LibraryMember(String name) {
 		this.name = name;
 		this.id = ++totalMembers;
 		System.out.println("Member registered: "+name+", ID: "+id);
 	}
+	//static method
 	public static void getTotalMembers() {
 		System.out.println("Total members: "+totalMembers);
 	}
+	//non static method
 	public void showMemberDetails() {
 		System.out.println("Name :"+name);
 		System.out.println("ID :"+ id);
--- Lolipop/src/May14/LibrarySystem.java
+++ Lolipop/src/May14/LibrarySystem.java
@@ -4,16 +4,22 @@ import java.util.Scanner;
 public class LibrarySystem {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		
 		LibraryMember.getTotalMembers();
+		
 		System.out.println("Enter the name of the member: ");
 		String name1 = scan.nextLine();
 		LibraryMember m1 = new LibraryMember(name1);
+		
 		System.out.println("Enter the name of the member: ");
 		String name2 = scan.nextLine();
 		LibraryMember m2 = new LibraryMember(name2);
+		
 		m1.showMemberDetails();
 		m2.showMemberDetails();
+		
 		LibraryMember.getTotalMembers();
+		
 		scan.close();
 	}
 }
