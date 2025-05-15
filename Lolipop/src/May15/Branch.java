8d3ffd8b54311203a3e9eb378f827a803a653d35
SANJAY V <sanjay3005v@gmail.com>
2025-05-15 10:51:27 +0530

    May 15

--- Lolipop/src/May15/Branch.java
+++ Lolipop/src/May15/Branch.java
@@ -3,13 +3,14 @@ package May15;
 public class Branch {
 	private String manager_name;
 	private String ifsc;
-	
+	//getter methods
 	public String getMN() {
 		return this.manager_name;
 	}
 	public String getifsc() {
 		return this.ifsc;
 	}
+	//setter methods
 	public void setMN(String manager_name) {
 		this.manager_name = manager_name;
 	}
