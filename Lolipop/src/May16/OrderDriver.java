aecb5b51da30d0fb7a98f977e1c63d567cdfb3d5
SANJAY V <sanjay3005v@gmail.com>
2025-05-16 10:41:15 +0530

    May 16

--- Lolipop/src/May16/OrderDriver.java
+++ Lolipop/src/May16/OrderDriver.java
@@ -7,6 +7,15 @@ public class OrderDriver {
 		Order order2 = new Order();
 		order2.setdetails("GoPro_hero11","5r5t45wwe5",52000,"Cash On delivery");
 		System.out.println("Order one details are ");
+		System.out.println("Product name : "+order1.getProductName());
+		System.out.println("Id : "+order1.getid());
+		System.out.println("Price : "+order1.getprice());
+		System.out.println("Payment : "+order1.getpayment());
+		System.out.println("Order two details are ");
+		System.out.println("Product name : "+order2.getProductName());
+		System.out.println("Id : "+order2.getid());
+		System.out.println("Price : "+order2.getprice());
+		System.out.println("Payment : "+order2.getpayment());
 	}
 	
 }
