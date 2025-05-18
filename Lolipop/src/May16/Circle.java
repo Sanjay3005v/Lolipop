15f09c8b6845009dc07e026f3b3fdab1f5868b12
SANJAY V <sanjay3005v@gmail.com>
2025-05-18 14:53:39 +0530

    May 18

--- Lolipop/src/May16/Circle.java
+++ Lolipop/src/May16/Circle.java
@@ -2,10 +2,20 @@ package May16;
 
 public class Circle {
 	private int radius;
+	//setter
 	public void setRadius(int radius) {
 		this.radius=radius;
 	}
+	//getter
 	public int getRadius() {
 		return this.radius;
+	}
+	//constructor
+	private Circle(int radius) {
+		this.radius=radius;
+	}
+	public static Circle DisplayRef(int radius) {
+		Circle c1 = new Circle(radius);
+		return c1;
 	}
 }
--- Lolipop/src/May16/CircleDriver.java
+++ Lolipop/src/May16/CircleDriver.java
@@ -2,11 +2,10 @@ package May16;
 import java.util.Scanner;
 public class CircleDriver {
 	public static void main(String[] args) {
-		Circle c1  = new Circle();
 		System.out.println("Enter the radius : ");
 		Scanner scan = new Scanner(System.in);
 		int radius = scan.nextInt();
-		c1.setRadius(radius);
+		Circle c1 = Circle.DisplayRef(radius);
 		Circles.getArea(c1);
 		Circles.getCircumference(c1);
 		scan.close();
