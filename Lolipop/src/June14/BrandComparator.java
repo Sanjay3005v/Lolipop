4767d1c6f8f1432a794f0a0f9730e4feb579a7a0
SANJAY V <sanjay3005v@gmail.com>
2025-06-15 11:32:26 +0530

    June15

--- Lolipop/src/June14/BrandComparator.java
+++ Lolipop/src/June14/BrandComparator.java
@@ -2,12 +2,10 @@ package June14;
 
 import java.util.Comparator;
 
-public class BrandComparator implements Comparator{
+public class BrandComparator implements Comparator<Laptop>{
 	@Override
-	public int compare(Object o1, Object o2) {
+	public int compare(Laptop o1, Laptop o2) {
 		// TODO Auto-generated method stub
-		Laptop l1 = (Laptop)o1;
-		Laptop l2 = (Laptop)o2;
-		return l1.brand.compareTo(l2.brand);
+		return o1.brand.compareTo(o2.brand);
 	}
 }
